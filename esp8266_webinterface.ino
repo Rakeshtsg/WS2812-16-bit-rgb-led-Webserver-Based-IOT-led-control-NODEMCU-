1fd3aa20cb8dca599ae81e96e3ee012fed20fc4c
RAKESH T.S <109905492+Rakeshtsg@users.noreply.github.com>
2023-04-11 20:03:32 +0530

    Update esp8266_webinterface.ino

--- esp8266_webinterface.ino
+++ esp8266_webinterface.ino
@@ -1,44 +1,4 @@
-/*
-  WS2812FX Webinterface.
-  
-  Harm Aldick - 2016
-  www.aldick.org
 
-  
-  FEATURES
-    * Webinterface with mode, color, speed and brightness selectors
-
-
-  LICENSE
-
-  The MIT License (MIT)
-
-  Copyright (c) 2016  Harm Aldick 
-
-  Permission is hereby granted, free of charge, to any person obtaining a copy
-  of this software and associated documentation files (the "Software"), to deal
-  in the Software without restriction, including without limitation the rights
-  to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
-  copies of the Software, and to permit persons to whom the Software is
-  furnished to do so, subject to the following conditions:
-
-  The above copyright notice and this permission notice shall be included in
-  all copies or substantial portions of the Software.
-
-  THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
-  IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
-  FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
-  AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
-  LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
-  OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN
-  THE SOFTWARE.
-
-  
-  CHANGELOG
-  2016-11-26 initial version
-  2018-01-06 added custom effects list option and auto-cycle feature
-  
-*/
 #ifdef ARDUINO_ARCH_ESP32
   #include <WiFi.h>
   #include <WebServer.h>
